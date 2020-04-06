9550d77d9c79c29ddfee148f31e48b71edd8b9c6
Deyve Vieira <deyvedvm@hotmail.com>
2020-04-05 21:25:43 -0300

    Implementing JWT

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,16 +1,20 @@
 import express, {Request, Response} from 'express';
 import {check, validationResult} from 'express-validator';
+import config from 'config';
 import bcrypt from 'bcryptjs';
 import gravatar from 'gravatar';
+import jwt from 'jsonwebtoken';
+import ramda from 'ramda';
 
 import User from '../models/User';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const users = await User.find({});
 
-    return res.send(users);
+    const users = await User.find().sort('name');
+
+    return res.send(users.map(user => pickFields(user)));
 });
 
 router.post('/', [
@@ -41,13 +45,32 @@ router.post('/', [
 
         await user.save();
 
-        return res.send('User saved');
+        const token = generateToken({id: user.id});
+
+        return res.header('x-auth-token', token).send(pickFields(user));
 
     } catch (e) {
         console.error(`Error ${e.message}  when try to find email`);
         return res.status(500).send(`Error when try to find email: ${email}`);
     }
 });
+
+router.delete('/:id', async (req: Request, res: Response) => {
+
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) return res.status(404).send(`The user not found with the id ${req.params.id}`);
+
+    return res.send(pickFields(user));
+});
+
+function generateToken(payload: any) {
+    return jwt.sign(payload, config.get('jwtSecret'), {expiresIn: '360000'});
+}
+
+function pickFields(user: any) {
+    return ramda.pick(['id', 'name', 'email', 'avatar', 'date'], user)
+}
 
 function createUserModel(name: string, email: string, avatar: string, password: string) {
     return new User({
